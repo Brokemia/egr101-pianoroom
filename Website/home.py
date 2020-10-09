2d1555ff3e4f3a472c7883442d829d3c16a88067
Ricky Weerts III <richard.weerts@duke.edu>
2020-10-09 09:29:29 -0400

    Implemented basic functionality

--- Website/home.py
+++ Website/home.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import json
 app = Flask(__name__)
 
-fname = 'test.json'
+fname = 'occupancy.json'
 
 @app.route('/room<int:roomNo>')
-def home(roomNo):
+def room(roomNo):
     with open(fname) as f:
         data = json.load(f)
         return f"Room {roomNo} is {'closed' if data[f'room{roomNo}'] else 'open'}"
@@ -13,12 +13,22 @@ def home(roomNo):
 @app.route('/data')
 def get_data():
     roomNo = int(request.args.get('r'))
-    full = bool(request.args.get('f'))
-    with open(fname, 'w+') as f:
+    full = bool(int(request.args.get('f')))
+    with open(fname) as f:
         try:
             data = json.load(f)
         except ValueError:
             data = {"room1":False,"room2":False,"room3":False,"room4":False}
-        data[f'room{roomNo}'] = full
+    data[f'room{roomNo}'] = full
+    with open(fname, 'w') as f:
         json.dump(data, f)
     return "Data set"
+
+@app.route('/')
+def home():
+    with open(fname) as f:
+        data = json.load(f)
+        return render_template('index.html', room1=data['room1'], room2=data['room2'], room3=data['room3'], room4=data['room4'])
+
+if __name__ == '__main__':
+    app.run()
