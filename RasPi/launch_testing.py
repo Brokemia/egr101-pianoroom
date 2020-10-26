abad62d9429e915c85d444aab9b7e241b3899d0b
AndrewCouncil <andrew.p.council@gmail.com>
2020-10-25 22:22:35 -0400

    Fixed timing and minor improvements

--- RasPi/launch_testing.py
+++ RasPi/launch_testing.py
@@ -5,7 +5,8 @@ from time import sleep
 import requests, os
 
 SENSE_DELAY_SECS = 120 #Seconds to delay after sensing before going inactive
-MAXIMUM_DISTANCE = 1 #Distance in meters
+DISTANCE_RESET_SECS = 3*3600 #Seconds of no pir activity that will reset distance sensor
+DEFAULT_MAXIMUM_METERS = 1 #Distance in meters
 PROGRAM_HZ = 30 #update speed of program
 JAM_BUTTON_PIN = 7
 JAM_LED_PIN = 9
@@ -29,6 +30,8 @@ jam_pressed = False
 jam_was_pressed = False
 jam_on = False
 jam_was_on = False
+pir_not_detected_time = 0
+max_meters = DEFAULT_MAXIMUM_METERS
 
 pir_sensor = Button(PIR_PIN, True)
 distance_sensor = DistanceSensor(echo=DISTANCE_ECHO_PIN, trigger=DISTANCE_TRIGGER_PIN)
@@ -38,27 +41,36 @@ jam_led = LED(JAM_LED_PIN)
 jam_led.off()
 jam_button = Button(JAM_BUTTON_PIN, True)
 
-def network_error():
+def network_wait():
     print("network error!")
     while True:
         ping_result = os.popen('ping google.com').read()
         if "Received = 4" in ping_result:
-            r = requests.get(ROOT_URL)
-            if r.status_code < 300:
-                print("\nnetwork back online!")
-                return
+            try:
+                r = requests.get(ROOT_URL)
+                if r.status_code < 300:
+                    print("\nnetwork back online!")
+                    return
+            except:
+                print("request not received...")
+        else:
+            print("ping not working, likely no internet connection...")
         
         rgb_led.color = Color('yellow')
         sleep(PROGRAM_HZ/1000)
 
 def send_http(url):
     ping_result = os.popen('ping google.com').read()
-    if "Received = 4" not in ping_result: network_error()
-
-    r = requests.get(url)
-    if r.status_code >= 300: 
-        network_error()
+    if "Received = 4" not in ping_result: network_wait()
+    try:
         r = requests.get(url)
+        if r.status_code >= 300: 
+            network_wait()
+            r = requests.get(url)
+    except:
+        network_wait()
+        r = requests.get(url)
+    
 
 while True:
     rgb_led.color = Color('green')
@@ -67,23 +79,34 @@ while True:
     person_detected = False
     if pir_sensor.is_pressed():
         person_detected = True
-    if distance_sensor.distance() <= MAXIMUM_DISTANCE:
+        pir_not_detected_time = 0
+    else:
+        if pir_not_detected_time > DISTANCE_RESET_SECS*1000:
+            max_meters = distance_sensor.distance()
+            pir_not_detected_time = 0
+        else:
+            pir_not_detected_time += PROGRAM_HZ
+
+    if distance_sensor.distance() <= max_meters:
         person_detected = True
 
     if person_detected:
         person_present = True
         time_total = 0
     else:
-        time_total += PROGRAM_HZ
         if time_total > SENSE_DELAY_SECS * 1000:
             person_present = False
+        else:
+            time_total += PROGRAM_HZ
 
     if person_present != person_was_present:
-        print("change detected!")
+        print("person change detected!")
         request_url = ROOT_URL
         if person_present:
+            print("person on now")
             request_url += TRUE_ROOM_URL
         else:
+            print("person off now")
             request_url += FALSE_ROOM_URL
         
         send_http(request_url)
@@ -100,11 +123,14 @@ while True:
         jam_was_pressed = jam_pressed
 
     if jam_on != jam_was_on:
+        print("jam change detected!")
         jam_led.toggle()
         request_url = ROOT_URL
         if jam_on:
+            print("jam on now")
             request_url += TRUE_JAM_URL
         else:
+            print("jam off now")
             request_url += FALSE_JAM_URL
         
         send_http(request_url)
@@ -112,8 +138,3 @@ while True:
     jam_was_on = jam_on
 
     sleep(PROGRAM_HZ/1000)
-
-
-    
-
-print("I run now")
