be3220380e7cf89807f38cf5f0fcdf558503dc5e
Ricky Weerts III <richard.weerts@duke.edu>
2020-11-02 08:41:18 -0500

    Rudimentary heatmap (not fully functional)

--- Website/home.py
+++ Website/home.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for
 import json
 import time
+import datetime as dt
 
 UPLOAD_FOLDER = 'templates/images/'
 
@@ -22,14 +23,22 @@ def room(roomNo):
 def get_data():
     roomNo = int(request.args.get('r'))
     full = bool(int(request.args.get('f')))
-    with open(fname) as f:
-        try:
+    try:
+        with open(fname) as f:
             data = json.load(f)
-        except ValueError:
-            currTime = time.time()
-            data = {"room1":False,"room2":False,"room3":False,"room4":False,"time1":currTime,"time2":currTime,"time3":currTime,"time4":currTime}
-            
-    if data[f'room{roomNo}'] and not(full):
+    except (ValueError,FileNotFoundError):
+        currTime = time.time()
+        chartData = []
+        for i in range(7):
+            chartData.append([0,0,0,0,0,0,0,0,0,0,0,0])
+        data = {"room1":False,"room2":False,"room3":False,"room4":False,"time1":currTime,"time2":currTime,"time3":currTime,"time4":currTime,"chartData":chartData}
+    
+    # Update the timer whenever it's full
+    if data[f'room{roomNo}']:
+        now = dt.datetime.now()
+        # Check how long since the last update
+        data["chartData"][now.weekday()][now.hour // 2] += time.time() - data[f'time{roomNo}']
+    
         data[f'time{roomNo}'] = time.time()
             
     data[f'room{roomNo}'] = full
@@ -50,7 +59,7 @@ def home():
             timeDiff = int(time.time() - recordedTime)
             data[f'time{roomNo}'] = str(time.strftime('%H:%M:%S', time.gmtime(timeDiff)))
     return render_template('index.html', room1=data['room1'], room2=data['room2'], room3=data['room3'], room4=data['room4'],
-                                time1=data["time1"], time2=data['time2'], time3=data['time3'], time4=data['time4'])
+                                time1=data["time1"], time2=data['time2'], time3=data['time3'], time4=data['time4'], chartData=data['chartData'])
 
 @app.route('/images/<filename>')
 def display_image(filename):
